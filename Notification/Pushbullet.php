a1f44d40c455fd6520b20fac5950efc738a9099b
Marijn Vandevoorde <marijn@marijnworks.be>
2015-06-26 15:05:02 +0200

    Bunch of fixes for notification methods. added ifttt

--- Notification/Pushbullet.php
+++ Notification/Pushbullet.php
@@ -9,12 +9,15 @@
  */
 namespace Sevenedge\Notification;
 
+use Cake\Core\Exception\Exception;
+use Cake\Network\Exception\BadRequestException;
 use Sevenedge\Utilities\CurlRequest;
 
 class Pushbullet
 {
 
-	CONST ENDPOINT_PUSH = 'https://{token}@api.pushbullet.com/v2/pushes';
+	CONST ENDPOINT_PUSH = 'https://api.pushbullet.com/v2/pushes';
+	CONST ENDPOINT_CONTACTLIST = 'https://api.pushbullet.com/v2/contacts';
 
 	private $_senderAuth;
 
@@ -38,19 +41,51 @@ class Pushbullet
 
 		$cr = new CurlRequest();
 		foreach ($recipients as $recipient) {
-			$postData['client_iden'] = $recipient;
-			$keys[$recipient] = $cr->addRequest(self::ENDPOINT_PUSH, json_encode($postData), array('Content-Type: application/json'), true, array(CURLOPT_POST => true, CURLOPT_BINARYTRANSFER => 1, CURLOPT_HEADER => true, CURLOPT_USERPWD => $this->_senderAuth));
+			$postData['email'] = $recipient;
+			$keys[$recipient] = $cr->addRequest(self::ENDPOINT_PUSH,
+				json_encode($postData),
+				array('Content-Type: application/json',  'Authorization: Bearer ' . $this->_senderAuth),
+				true,
+				array(CURLOPT_POST => true, CURLOPT_BINARYTRANSFER => 1, CURLOPT_HEADER => true));
 		}
 		$cr->execute();
 
 
 		foreach ($keys as $recipient => $key) {
 			$msg = $cr->getResponse($key);
-			echo $recipient . " ----> ";
-			var_dump(json_decode($msg['response'], 1));
-
-			echo "\n------\n-----\n";
+			if ($msg['http_code'] === 200) {
+				$keys[$recipient] = array('status' => 'success');
+			} else {
+				$keys[$recipient] = array('status' => 'error', 'error' => $msg['error']);
+			}
 		}
 		$cr->__destruct();
+
+		return $keys;
+	}
+
+	/**
+	 * @return mixed array of contacts
+	 * @throws BadRequestException in case the request was bad. contains http code and error message.
+	 */
+	public function getContacts() {
+		$cr = new CurlRequest();
+		$key = $cr->addRequest(self::ENDPOINT_CONTACTLIST, array(),
+			array('Content-Type: application/json',  'Authorization: Bearer ' . $this->_senderAuth),
+			true, array(CURLOPT_BINARYTRANSFER => 1, CURLOPT_HEADER => true));
+		$response = $cr->getResponse($key);
+		if ($cr->execute() === 0) {
+			$response['response'] = json_decode($response['response'], 1);
+			if ($response['http_code'] === 200) {
+				return $response['response']['contacts'];
+			} else {
+				throw new BadRequestException($response['response']['error']['message'], $response['http_code']);
+			}
+
+		}
+		else {
+			throw new BadRequestException($response['error'], $response['http_code']);
+		}
+
 	}
 }

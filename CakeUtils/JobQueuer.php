c69c88caf7f3ada754109bfd59c0a14a1400ead6
Rein Deneut <rein@sevenedge.be>
2015-09-22 09:21:21 +0200

    go to next task if current task fails (best to not throw an exception, because the MySQL connection did not close upon Exception)

--- CakeUtils/JobQueuer.php
+++ CakeUtils/JobQueuer.php
@@ -2,24 +2,25 @@
 
     namespace Sevenedge\CakeUtils;
 
-	App::uses('Job', 'Model');
+	use App\Model\Entity\Job;
+	use Cake\Core\App;
+	use Cake\ORM\TableRegistry;
 
 	class JobQueuer {
 
 
 		public static function queue($task, $params = array(), $maxtime = 600) {
-			$Job = new Job();
-			$data = $Job->create();
-			$data['Job']['task'] = $task;
-			$data['Job']['params'] = serialize($params);
-			$data['Job']['maxtime'] = $maxtime;
-
-			$Job->save($data);
+			$jobs = TableRegistry::get('Jobs');
+			$data = $jobs->newEntity();
+			$data->task = $task;
+			$data->params = serialize($params);
+			$data->maxtime = $maxtime;
+			$jobs->save($data);
 		}
 
 		public static function isEmpty() {
-			$Job = new Job();
-			$res = $Job->find('first', array('conditions' => 'executed IS NULL'));
+			$jobs = TableRegistry::get('Jobs');
+			$res = $jobs->find('all', array('conditions' => 'executed IS NULL'))->first();
 			return empty($res);
 		}
 
@@ -27,81 +28,89 @@
 		 * We're only doing one job at a time. This to prevent really long execution times
 		 */
 		public static function handle() {
-			$Job = new Job();
+			$jobs = TableRegistry::get('Jobs');
 			$now = time();
 
-			$todo = $Job->find('first', array('conditions' =>
+			$todos = $jobs->find('all', array('conditions' =>
 				array('OR' =>
 					array(
-						'Job.started' => null,
-						/*
+						'started IS NULL',
 						'AND' => array(
-							'UNIX_TIMESTAMP(Job.started) + Job.maxtime < ' . $now,
-							'Job.executed' => null
+							'UNIX_TIMESTAMP(started) + maxtime < ' . $now,
+							'executed IS NULL'
 						)
-						*/
+
 				)),
 
-				'order' => array('Job.id ASC')
-			));
+				'order' => array('id' =>'ASC')
+			))->toArray();
 
 
-			if(!empty($todo)) {
-				$todo['Job']['started'] = date('Y-m-d H:i:s');
-				$Job->save($todo);
-				try {
-					$params = unserialize($todo['Job']['params']);
-					$params = $params? $params : array();
+			foreach ($todos as $todo) {
+				if (!empty($todo)) {
 
-					$type = 'coremethod';
+					$todo->started = date('Y-m-d H:i:s');
+					$jobs->save($todo);
+					try {
+						$params = unserialize($todo->params);
+						$params = $params ? $params : array();
 
-					$task = explode(".", $todo['Job']['task']);
-					if (count($task) > 1) {
-						$type = 'instance';
-					} else {
-						$task = explode("::", $todo['Job']['params']);
+						$type = 'coremethod';
+
+						$task = explode(".", $todo->task);
+
 						if (count($task) > 1) {
-							$type = 'static';
+							$type = 'instance';
 						}
-					}
-
-					// We might have to include some files here.
-					if (in_array($type, array('instance', 'static'))) {
-						// it's a class. and it's not static!
-						$class = array_shift($task);
-						$class = explode("/", $class);
-						$className = array_pop($class);
-						if (count($class) > 0) {
-							// it's some lib
-							$path = implode('/', $class);
-							App::uses($className, $path);
+						else {
+							$task = explode("::", $todo->task);
+							if (count($task) > 1) {
+								$type = 'static';
+							}
 						}
-					}
-					$task = array_shift($task);
-					switch ($type) {
-						case 'static':
-							call_user_func_array($className . "::" . $task, $params);
-							break;
-						case 'instance':
-							//TODO might want to add some paramters for the constructor in the future, right? Even though not needed in this project.
-                            // HOWTO: split up params. make it an assoc array with 'init' and 'call' params.
-							$instance = new $className();
-							call_user_func_array(array($instance, $task), $params);
-							break;
-						case 'coremethod':
-							call_user_func_array($task, $params);
-							break;
-						default:
-							throw new NotImplementedException("can't find that job:" . print_r($todo, 1));
-					}
 
-					$todo['Job']['executed'] = date('Y-m-d H:i:s');
-					$Job->save($todo);
-				}
-				catch (Exception $e) {
-					$todo['Job']['started'] = null;
-					$Job->save($todo);
-					throw $e;
+						// We might have to include some files here.
+						if (in_array($type, array('instance', 'static'))) {
+							// it's a class. and it's not static!
+							$class = array_shift($task);
+
+						}
+						$task = array_shift($task);
+
+						$doContinue = false;
+
+						switch ($type) {
+							case 'static':
+								$doContinue = (!call_user_func_array($class . "::" . $task, $params)) ? true : false;
+								break;
+							case 'instance':
+								//TODO might want to add some paramters for the constructor in the future, right? Even though not needed in this project.
+								// HOWTO: split up params. make it an assoc array with 'init' and 'call' params.
+								$instance = new $class();
+								call_user_func_array(array(
+									$instance,
+									$task
+								), $params);
+								break;
+							case 'coremethod':
+								call_user_func_array($task, $params);
+								break;
+							default:
+								throw new NotImplementedException("can't find that job:" . print_r($todo, 1));
+						}
+						
+						// If we cant login to the server,
+						// go to next job
+						if ($doContinue) {
+							continue;
+						}
+
+						$todo->executed = date('Y-m-d H:i:s');
+						$jobs->save($todo);
+					} catch (Exception $e) {
+						$todo->started = NULL;
+						$jobs->save($todo);
+					}
 				}
 			}
 		}
